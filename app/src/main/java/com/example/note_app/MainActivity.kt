da637bfd50fe3e6a4ec1a0ccbef1de57423025be
arya_irse@yahoo.com <arya_irse@yahoo.com>
2023-11-01 16:35:45 +0100

    Started again from beginning, i hope to succe this time

--- app/src/main/java/com/example/note_app/MainActivity.kt
+++ app/src/main/java/com/example/note_app/MainActivity.kt
@@ -8,6 +8,8 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.note_app.ui.theme.Note_AppTheme
@@ -16,13 +18,15 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val list = remember { mutableStateListOf<Objekt>()}
             Note_AppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
+                    
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Screen()
+                    MainScreen(list = list)
                 }
             }
         }
@@ -35,6 +39,6 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun GreetingPreview() {
     Note_AppTheme {
-        Screen()
+        
     }
 }
--- app/src/main/java/com/example/note_app/Screen.kt
+++ app/src/main/java/com/example/note_app/Screen.kt
@@ -1,131 +1,72 @@
 package com.example.note_app
 
-import android.graphics.Paint.Style
-import android.widget.ListView
-import androidx.compose.animation.VectorConverter
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.AnnotatedString
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.PreviewParameter
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import androidx.lifecycle.viewmodel.CreationExtras
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.rememberNavController
-import kotlin.collections.isNotEmpty
 
 
-
+class Objekt(val title: String, val text: String)
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Screen(){
-    var Title by rememberSaveable { mutableStateOf("") }
-    var text by rememberSaveable { mutableStateOf("") }
-    var SavedText by rememberSaveable { mutableStateOf("") }
-    var SavedTitle by rememberSaveable { mutableStateOf("") }
-    var SavedTextVisible by rememberSaveable { mutableStateOf(false) }
-    var savedTitleVisible by rememberSaveable { mutableStateOf(false) }
-    var IsEditing by rememberSaveable { mutableStateOf(false) }
-    var Editing by rememberSaveable { mutableStateOf(false) }
-    var MyTitle = listOf(SavedTitle)
-    var MyList = listOf(SavedText)
-    var SaveButtonPressed by rememberSaveable { mutableStateOf(false) }
+fun MainScreen(list: MutableList<Objekt>, modifier: Modifier=Modifier){
 
-    Row (modifier = Modifier
-        .background(Color.Red)
-        .fillMaxSize()){
-        Column(modifier = Modifier
-            .background(Color.DarkGray)
-            .fillMaxSize()) {
-
-            Text(text = "Title")
-            TextField(value = Title, onValueChange = {Title=it})
-            Text(text = "Text")
-            TextField(value = text, onValueChange ={text=it} )
-            Row {
-                Button(
-                    onClick = {
-                        SavedText = text
-                        SavedTitle = Title
-                        Title=""
-                        text=""
-                        SavedTextVisible=true
-                        savedTitleVisible=true
-                        SaveButtonPressed=true
-
-                    })
-                {
-                    Text(text = "Save")
-
-                }
-
-                Button(
-                    onClick = {
-                        text = SavedText
-                        Title=SavedTitle
-                    } ) {
-                    Text(text = "Edit")
-                }
-            }
-            Column(modifier = Modifier
-                .background(Color.Green)
-                .fillMaxSize()) {
-                if (SaveButtonPressed){
-                    Column {
-                        Text(text = "Title: ", style = TextStyle(color= Color.Red, fontSize = 20.sp, background = (Color.Black)))
-                        MyTitle.forEach { title ->
-                            Text(
-
-                                text = AnnotatedString(title),
-                                style = TextStyle(color = Color.Black)
-                            )
-                        }
-                        MyList.forEach{ item ->
-                            Text(text = "Note: ", style = TextStyle(color= Color.Red, fontSize = 20.sp,  background = (Color.Black)))
-                            Text(
-                                text = AnnotatedString(item),
-                                style = TextStyle(color = Color.Black))
-                        }
-                    }
-
-                }
-
-
-
-
-
-            }
-            }
-
-
+    Column {
+        List( list = list)
+        ListView(list = list)
 
     }
 
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun List(list: MutableList<Objekt>) {
+
+    var title by rememberSaveable { mutableStateOf("") }
+    var text by rememberSaveable { mutableStateOf("") }
+    Column {
+        TextField(value = title, onValueChange = { NewTitle ->
+            title=NewTitle
+        }, label = { Text(text = "Tiltle: ")}
+        )
+        TextField(value = text, onValueChange = { NewText ->
+            text=NewText
+        }, label = { Text(text = "Text: ")}
+        )
+        Button(onClick = {
+            list.add(Objekt(title=title, text=text))
+            println(list)
+            title=""
+            text=""
+        }) {
+            Text(text = "Save")
+
+        }
+    }
+    
+}
+
+@Composable
+fun ListView(list: MutableList<Objekt>) {
+    LazyColumn{
+        items(list){ThisObject ->
+            Column {
+                Text(text = ThisObject.title )
+                Text(text = ThisObject.text)
+            }
 
 
+        }
+    }
+    
+}
