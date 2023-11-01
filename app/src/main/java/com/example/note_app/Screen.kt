f0ae9929a7ee4a6874bd6bb73815a708ace30529
arya_irse@yahoo.com <arya_irse@yahoo.com>
2023-11-01 01:28:20 +0100

    I have no idea what i am doing now, will continue tomorrow again

--- app/src/main/java/com/example/note_app/Screen.kt
+++ app/src/main/java/com/example/note_app/Screen.kt
@@ -1,30 +1,39 @@
 package com.example.note_app
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import org.w3c.dom.Text
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import kotlin.collections.isNotEmpty
+
+
 
 @OptIn(ExperimentalMaterial3Api::class)
-@Preview
 @Composable
 fun Screen(){
     var Title by rememberSaveable { mutableStateOf("") }
@@ -33,25 +42,44 @@ fun Screen(){
     var SavedTitle by rememberSaveable { mutableStateOf("") }
     var SavedTextVisible by rememberSaveable { mutableStateOf(false) }
     var savedTitleVisible by rememberSaveable { mutableStateOf(false) }
-    Row (modifier = Modifier.padding(16.dp)){
-        Column() {
+    var IsEditing by rememberSaveable { mutableStateOf(false) }
+    var Editing by rememberSaveable { mutableStateOf(false) }
+    Row (modifier = Modifier
+        .background(Color.Red)
+        .fillMaxSize()){
+        Column(modifier = Modifier
+            .background(Color.DarkGray)
+            .fillMaxSize()) {
+
             Text(text = "Title")
             TextField(value = Title, onValueChange = {Title=it})
             Text(text = "Text")
             TextField(value = text, onValueChange ={text=it} )
-            Button(
-                onClick = {
-                    SavedText = text
-                    SavedTitle = Title
-                    Title=""
-                    text=""
-                    SavedTextVisible=true
-                    savedTitleVisible=true
-                })
-            {
-                Text(text = "Save")
+            Row {
+                Button(
+                    onClick = {
+                        SavedText = text
+                        SavedTitle = Title
+                        Title=""
+                        text=""
+                        SavedTextVisible=true
+                        savedTitleVisible=true
+
+                    })
+                {
+                    Text(text = "Save")
+                }
+                Button(
+                    onClick = {
+                        text = SavedText
+                        Title=SavedTitle
+                    } ) {
+                    Text(text = "Get-Note")
+                }
             }
-        }
+            }
+
 
     }
+
 }
